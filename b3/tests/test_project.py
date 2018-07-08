43f2bac9048390ef08ee0d66c04c3083fcf1c948
Luke <lsh@ccxx.cx>
2018-07-08 13:31:05 +0930

    more tests for defaults with extended types

--- b3/tests/test_project.py
+++ b3/tests/test_project.py
@@ -62,3 +62,54 @@ def test_basic_project_extended_defaults_overrides():
     actual = project.project_data('basic-defaults-extended-w-overrides', oname='test-project')
     assert expected == actual
 
+def test_project_extended_defaults():
+    expected = {
+        'resource1': {
+            'os': 'ubuntu-16.04', 'size': 'tiny',
+            'extra-volumes': [
+                {
+                    'size': 10,
+                    'dev': '/dev/xvda',
+                    'mnt': '/mnt/xvdh',
+                },
+                {
+                    'size': 10,
+                    'dev': '/dev/xvdh',
+                    'mnt': '/mnt/xvdb',
+                },
+                {
+                    'size': 100,
+                    'dev': '/dev/xvdh',
+                    'mnt': '/mnt/xvdh'
+                }
+            ]
+        }
+    }
+    actual = project.project_data('extended-defaults', oname='test-project-2')
+    assert expected == actual
+
+def test_project_extended_defaults_overrides():
+    expected = {
+        'resource1': {
+            'os': 'ubuntu-16.04', 'size': 'tiny',
+            'extra-volumes': []
+        }
+    }
+    actual = project.project_data('extended-defaults-w-overrides', oname='test-project-2')
+    assert expected == actual
+
+def test_basic_project_extended_defaults_overrides():
+    expected = {
+        'resource1': {
+            'os': 'ubuntu-16.04', 'size': 'tiny',
+            'extra-volumes': [
+                {
+                    'size': 10,
+                    'dev': '/dev/xvdh',
+                    'mnt': '/mnt/xvdh'
+                }
+            ]
+        }
+    }
+    actual = project.project_data('extended-defaults-w-overrides2', oname='test-project-2')
+    assert expected == actual
