40af1e83dd6c2cb9a76725cb6bd3f4f2bf0e8831
Luke <lsh@ccxx.cx>
2018-07-21 13:28:19 +0930

    adds ansible.py and support for generating an inventory file

---
+++ b3/ansible.py
@@ -0,0 +1,54 @@
+import os
+from . import conf, project
+from .utils import ensure
+from collections import OrderedDict
+import configparser
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+def read_inventory(path=None):
+    path = path or conf.INVENTORY_FILE
+    if not os.path.exists(path):
+        LOG.warn("inventory file not found: %s" % path)
+        return OrderedDict()
+    inv = configparser.ConfigParser()
+    inv.read_file(open(path, 'r'))
+    # we accept and return simple datastructures, not ConfigParser instances
+    return OrderedDict([(section, OrderedDict(section_body)) for section, section_body in inv.items()])
+
+def write_inventory(invdict, path=None):
+    ensure(isinstance(invdict, dict), "given inventory is not a dictionary")
+    path = path or conf.INVENTORY_FILE
+    inv = configparser.ConfigParser()
+    inv.read_dict(invdict)
+    with open(path, 'w') as configfile:
+        inv.write(configfile)
+    return path
+
+def instance_groups(iid):
+    pname, iname = project.parse_iid(iid)[:2]
+    return [
+        'all',
+        pname, # project 'foo'
+        iname, # instance 'bar' or 'prod' or 'staging'
+        iid, # a group of it's own
+    ]
+
+def add_to_inventory(iid, path=None):
+    # if shared inventory, sync inventory first
+    inv = read_inventory()
+    group_list = instance_groups(iid)
+
+    # todo: pull these from the instance statefile
+    public_ip = '0.0.0.0'
+    ssh_port = 22
+
+    for group_name in group_list:
+        grp = inv.get(group_name, OrderedDict())
+        grp[iid] = "%s:%s" % (public_ip, ssh_port)
+        inv[group_name] = grp
+
+    write_inventory(inv, path)
+    return inv
--- b3/conf.py
+++ b3/conf.py
@@ -3,9 +3,24 @@ from os.path import join
 SRC_DIR = os.path.realpath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.dirname(SRC_DIR)
 ORG_DIR = join(PROJECT_DIR, "projects")
-INSTANCE_DIR = join(PROJECT_DIR, 'instances')
 DEFAULT_PROJECT_FILE = 'default'
 
+#
+# terraform
+#
+
+INSTANCE_DIR = join(PROJECT_DIR, 'instances')
+
+#
 # testing
+#
+
 TEST_DIR = join(SRC_DIR, 'tests')
 TEST_FIXTURE_DIR = join(TEST_DIR, 'fixtures')
+
+#
+# ansible
+#
+
+# a simple list of all known instances, grouped in different ways 
+INVENTORY_FILE = join(PROJECT_DIR, 'inventory')
