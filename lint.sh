60b3e6a369b3ab6912d1453ac83d87f91b6a0db8
Luke <lsh@ccxx.cx>
2018-08-13 00:50:55 +0930

    added exclusion to lint file

--- lint.sh
+++ lint.sh
@@ -2,4 +2,7 @@
 set -e
 source venv/bin/activate
 pyflakes tasks/ b3/
-pylint -E tasks/ b3/
+
+# E1101=no-member, temporary while python2 is still supported
+pylint -E tasks/ b3/ \
+    --disable=E1101
