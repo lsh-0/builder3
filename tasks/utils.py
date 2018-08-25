1c5f53bc163952121c8325bb5f43caa4238a8ddf
Luke <lsh@ccxx.cx>
2018-08-25 14:16:42 +0930

    fixed up utils.picj

--- tasks/utils.py
+++ tasks/utils.py
@@ -2,21 +2,35 @@ from b3.utils import ensure
 
 def pick(choice_type, choices, default=0xDEADBEEF):
     ensure(choices, "no %s to choose from" % choice_type)
-    default != 0xDEADBEEF and ensure(default in choices, "default (%s) isn't in list of choices (%s)" % (default, choices))
+    has_default = default != 0xDEADBEEF
     if len(choices) == 1:
         return choices[0]
+    has_default and ensure(default in choices, "default (%r) isn't in list of choices (%s)" % (default, choices))
+
     print("%s:" % choice_type)
     for i, x in enumerate(choices):
-        print(i, x)
+        print("[%s] %s" % (i, x))
+
     while True:
-        uin = input('> ')
+        prompt = '> '
+        if has_default:
+            prompt = "(%s) > " % default
+
+        uin = input(prompt)
         if not uin:
             if default != 0xDEADBEEF:
                 return default
             print('input required')
             continue
+
         uin = uin.strip()
         if not uin.isdigit():
-            print('a number between 0 and %s is required' % len(choices) - 1)
+            print('a number between 0 and %s is required' % (len(choices) - 1,))
             continue
-        return choices[int(uin)]
+
+        uin = int(uin)
+        if not uin in range(0, len(choices)):
+            print('a number between 0 and %s is required' % (len(choices) - 1,))
+            continue
+
+        return choices[uin]
