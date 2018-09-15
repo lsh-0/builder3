7e34a31805b5bcfaa22247f224be0667a98f408a
Luke <lsh@ccxx.cx>
2018-09-15 10:43:33 +0930

    notes to self

--- b3/keypair.py
+++ b3/keypair.py
@@ -10,6 +10,8 @@ def keypair_path(iid):
     path = project.instance_path(iid, create_dirs=True)
     pem_fname = iid + "_rsa"
     pub_fname = pem_fname + ".pub"
+    # absolute paths. is this what I want? 
+    # context.py shortens them, but they could be shortened right here
     return join(path, pub_fname), join(path, pem_fname)
 
 def create_keypair(iid):
@@ -24,14 +26,14 @@ def create_keypair(iid):
         return pubkey, pemkey
     # if pemkey exists, it *will* prompt you.
     cmd = "ssh-keygen -t rsa -f %s -N ''" % pemkey
-    rc = os.system(cmd)
+    rc = os.system(cmd) # TODO: replace with utils.local_cmd
     ensure(rc == 0, "failed to generate keypair for %s: %s" % (pemkey, rc))
     ensure(os.path.exists(pubkey), "pubkey not found: %s" % pubkey)
     ensure(os.path.exists(pemkey), "pemkey not found: %s" % pemkey)
 
     # correct permissions on pemkey else ssh barfs
     cmd = "chmod 400 %s" % pemkey
-    rc = os.system(cmd)
+    rc = os.system(cmd) # TODO: replace with utils.local_cmd
     ensure(rc == 0, "failed to set permissions on pemkey: %s" % pemkey)
 
     return pubkey, pemkey
