ba85a65ca1c29d94c46896722d679c5d5c08138c
Luke <lsh-0@users.noreply.github.com>
2018-09-30 11:03:11 +0930

    linter, removed a warning related to fabric code

--- lint.sh
+++ lint.sh
@@ -4,5 +4,6 @@ source venv/bin/activate
 pyflakes tasks/ b3/
 
 # E1101=no-member, temporary while python2 is still supported
+# E1129=Not Context Manager, fabric issue
 pylint -E tasks/ b3/ \
-    --disable=E1101
+    --disable=E1101,E1129
